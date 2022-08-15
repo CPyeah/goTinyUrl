8b8b7bf55e2f91caa5c850f6b236575f9f48a5e4
chengpeng <chengpengyeah@126.com>
2022-08-15 11:19:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tiny Url by Go
+![img.png](img.png)
+
+- go web framework: [Gin](https://github.com/gin-gonic/gin)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
+	router.POST("/form_post", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "posted",
+			"message": message,
+			"nick":    nick,
+		})
+	})
+
+	fmt.Println("http://localhost:8080/welcome?lastname=cp")
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
