c692ce2eb73f95ed8dfde4310afe097c431e4172
chengpeng <chengpengyeah@126.com>
2022-11-16 17:04:36 +0800

    add bloomfilter.go

--- cache/bloomfilter_test.go
+++ cache/bloomfilter_test.go
@@ -14,3 +14,47 @@ func TestNewRedisBloomFilter(t *testing.T) {
 	//exists, err = bloom.MightContain([]byte("hello1"))
 	fmt.Println(exists, err)
 }
+
+func TestGet(t *testing.T) {
+	r := maxCoins([]int{3, 1, 5, 8})
+	fmt.Println(r)
+}
+
+func maxCoins(nums []int) int {
+	maxPoint = 0
+	poke(nums, 0)
+	return maxPoint
+}
+
+var maxPoint int
+
+func poke(nums []int, point int) {
+	for i, _ := range nums {
+		point += getPoint(nums, i)
+		if point > maxPoint {
+			maxPoint = point
+		}
+		left := make([]int, 0)
+		right := make([]int, 0)
+		if i > 0 {
+			left = nums[:i-1]
+		}
+		if i < len(nums)-1 {
+			right = nums[i+1:]
+		}
+		newNums := append(left, right...)
+		poke(newNums, point)
+	}
+}
+
+func getPoint(nums []int, i int) int {
+	left := 1
+	if i > 0 {
+		left = nums[i-1]
+	}
+	right := 1
+	if i < len(nums)-1 {
+		right = nums[i+1]
+	}
+	return left * right * nums[i]
+}
